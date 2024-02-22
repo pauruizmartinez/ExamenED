791b04999c71a95e86ef65d901cc675b6843e2b0
pauruizmartinez <prm00@iesemilidarder.com>
2024-02-22 16:27:03 +0100

    Tercera versió

--- tests/tests/testsExamen.java
+++ tests/tests/testsExamen.java
@@ -1,14 +1,41 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;  
+import org.junit.BeforeClass; 
+import org.junit.Test; 
+import java.util.NoSuchElementException; 
 
-import org.junit.jupiter.api.Test;
+public class TestPrograma { 
 
-class testsExamen {
+    private static int[] elements;  
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @BeforeClass 
+    public static void BeforeClass(){ 
+        elements = new int[] {1, 2, 3, 4, 5}; 
+    } 
 
+    @Test 
+    public void testSumaTaula() {
+        TaulaEnters taula = new TaulaEnters(elements); 
+        assertEquals(15, taula.sumaTaula()); 
+    } 
+
+    @Test
+    public void testMajorTaula() { 
+        TaulaEnters taula = new TaulaEnters(elements); 
+        assertEquals(5, taula.majorTaula());  
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testPosicioTaulaNotFound() {
+        TaulaEnters taula = new TaulaEnters(elements);
+       
+        taula.posicioTaula(10); 
+    }
+
+    @Test
+    public void testPosicioTaulaFound() {
+        TaulaEnters taula = new TaulaEnters(elements);
+        assertEquals(2, taula.posicioTaula(3)); 
+    }
 }
